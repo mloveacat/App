f349bd8c455ce6b40217e64a7f448c68d07fa1d7
mloveacat <949929695@qq.com>
2016-06-30 16:09:03 +0800

    app的提交添加了button

--- app/src/main/java/com/cat/my/android1604/MainActivity.java
+++ app/src/main/java/com/cat/my/android1604/MainActivity.java
@@ -2,9 +2,10 @@ package com.cat.my.android1604;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+Button button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
